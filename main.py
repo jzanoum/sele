5b1fe59fafb2d6d159983ff28e7e7a3cfd64743d
jzanoum <141528988+jzanoum@users.noreply.github.com>
2023-09-08 08:39:52 +0200

    main.py

--- main.py
+++ main.py
@@ -1,51 +1,40 @@
-# Importer les bibliothèques
-import json
-from requestium import Session
-from flask import Flask, request
 
-# Créer une instance de l'application flask
-app = Flask(__name__)
+import json from selenium import webdriver from selenium.webdriver.chrome.options import Options import requests from flask import Flask, request
 
-# Définir une route pour le point de terminaison
-@app.route('/video', methods=['POST'])
-def video():
-    # Obtenir le contenu du fichier json en utilisant request.get_json()
-    data = request.get_json()
-    # Appeler la fonction qui traite le fichier json et renvoie le nom du fichier de sortie
-    output_file = process_json(data)
-    # Retourner le nom du fichier de sortie comme réponse
-    return output_file
+app = Flask(name)
 
-# Définir une fonction qui prend en entrée le contenu du fichier json et renvoie le nom du fichier de sortie
-def process_json(data):
-    # Extraire les informations du fichier json
-    url = data['url']
-    format = data['format']
-    folder = data['folder']
+@app.route(‘/video’, methods=[‘POST’]) def video(): # Obtenir le contenu du fichier json en utilisant request.get_json() data = request.get_json() # Appeler la fonction qui traite le fichier json et renvoie le nom du fichier de sortie output_file = process_json(data) # Retourner le nom du fichier de sortie comme réponse return output_file
 
-    # Utiliser requestium pour accéder à l'url de la vidéo
-    s = Session(webdriver_path='chromedriver',
-                browser='chrome',
-                default_timeout=15,
-                webdriver_options={'arguments': ['headless']})
-    s.get(url)
+Définir une fonction qui prend en entrée le contenu du fichier json et renvoie le nom du fichier de sortie
+def process_json(data): # Extraire les informations du fichier json url = data[‘url’] format = data[‘format’] folder = data[‘folder’]
 
-    # Utiliser requestium pour obtenir le contenu binaire de la vidéo
-    video_data = s.driver.get_screenshot_as_png()
+# Créer les options pour le navigateur chrome en mode headless
+options = Options()
+options.add_argument('--headless')
 
-    # Utiliser requestium pour obtenir le titre de la vidéo
-    video_title = s.driver.title
+# Utiliser selenium pour accéder à l'url de la vidéo avec un navigateur chrome en mode headless
+driver = webdriver.Chrome(options=options)
+driver.get(url)
 
-    # Créer le nom du fichier de sortie
-    output_file = folder + '/' + video_title + '.' + format
+# Utiliser selenium pour obtenir le titre de la vidéo
+video_title = driver.title
 
-    # Ouvrir le fichier de sortie en mode binaire
-    with open(output_file, 'wb') as f:
-        # Écrire les données de la vidéo dans le fichier
-        f.write(video_data)
+# Utiliser requests pour obtenir le contenu binaire de la vidéo en utilisant l'url du tag video
+video_data = requests.get(driver.find_element_by_tag_name('video').get_attribute('src')).content
 
-    # Retourner le nom du fichier de sortie
-    return output_file
+# Fermer le navigateur
+driver.quit()
+
+# Créer le nom du fichier de sortie
+output_file = folder + '/' + video_title + '.' + format
+
+# Ouvrir le fichier de sortie en mode binaire
+with open(output_file, 'wb') as f:
+    # Écrire les données de la vidéo dans le fichier
+    f.write(video_data)
+
+# Retourner le nom du fichier de sortie
+return output_file
 
 
 if __name__ == '__main__':
