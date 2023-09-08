99a1ca35733539e8a60e9aaae729d2b7eae47825
jzanoum <141528988+jzanoum@users.noreply.github.com>
2023-09-08 21:51:58 +0200

    main.py

--- main.py
+++ main.py
@@ -49,4 +49,4 @@ def process_json(data):
     return output_file
 
 if __name__ == '__main__':
-    app.run(debug=True, port=os.getenv("PORT", default=5000))
+    app.run(debug=True, host='0.0.0.0', port=os.getenv("PORT", default=5000))
