e87e343a81b9742028402886791120b3f290f6f7
jzanoum <141528988+jzanoum@users.noreply.github.com>
2023-09-19 01:16:17 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, request, jsonify
 import requests
 
@@ -8,10 +9,17 @@ def download_video():
     url = request.json['url']
     format = request.json['format']
     folder = request.json['folder']
+    
+    # Create the folder if it doesn't exist
+    if not os.path.exists(folder):
+        os.makedirs(folder)
+    
     response = requests.get(url)
     with open(f'{folder}/video.{format}', 'wb') as file:
         file.write(response.content)
+    
     return jsonify({'message': 'Video downloaded successfully!'})
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=os.getenv("PORT", default=5000))
+
