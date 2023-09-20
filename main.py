67fc2dad74d3295efc7f5653188a28a6b66e70fd
jzanoum <141528988+jzanoum@users.noreply.github.com>
2023-09-21 00:15:28 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import os
 from flask import Flask, request, jsonify
-import requests
-import validators
 
 app = Flask(__name__)
 
@@ -14,13 +13,20 @@ def download_video():
         return jsonify({'message': 'L\'URL fournie n\'est pas un lien vidéo valide.'})
     
     response = requests.get(url)
-    with open(f'{folder}/video.{format}', 'wb') as file:
+    
+    # Obtenez le répertoire de stockage externe sur Android
+    external_storage_dir = os.path.join(os.environ['EXTERNAL_STORAGE'], 'VotreApplication')
+    
+    # Créez un répertoire spécifique pour votre application
+    os.makedirs(external_storage_dir, exist_ok=True)
+    
+    # Enregistrez la vidéo téléchargée dans le répertoire spécifique
+    with open(os.path.join(external_storage_dir, f'video.{format}'), 'wb') as file:
         file.write(response.content)
     
     return jsonify({'message': 'Vidéo téléchargée avec succès !'})
 
-def is_valid_video_url(url):
-    return validators.url(url) and any(ext in url for ext in ['.mp4', '.avi', '.mov', '.wmv'])
+# Le reste du code...
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=os.getenv("PORT", default=5000))
