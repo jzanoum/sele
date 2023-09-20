2d9d10b508d775c016c47af90dd39e53d4717d46
jzanoum <141528988+jzanoum@users.noreply.github.com>
2023-09-20 16:54:01 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
 from flask import Flask, request, jsonify
-import requests
+import validators
 
 app = Flask(__name__)
 
@@ -10,16 +9,17 @@ def download_video():
     format = request.json['format']
     folder = request.json['folder']
     
-    # Create the folder if it doesn't exist
-    if not os.path.exists(folder):
-        os.makedirs(folder)
+    if not is_valid_video_url(url):
+        return jsonify({'message': 'L\'URL fournie n\'est pas un lien vidéo valide.'})
     
     response = requests.get(url)
     with open(f'{folder}/video.{format}', 'wb') as file:
         file.write(response.content)
     
-    return jsonify({'message': 'Video downloaded successfully!'})
+    return jsonify({'message': 'Vidéo téléchargée avec succès !'})
+
+def is_valid_video_url(url):
+    return validators.url(url) and any(ext in url for ext in ['.mp4', '.avi', '.mov', '.wmv'])
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=os.getenv("PORT", default=5000))
-
